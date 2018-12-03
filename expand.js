67791b28aea1fa8720dcf6c4323460a9668dad70
Samuel Alonso <alonso.smontenegro@gmail.com>
2018-12-03 00:27:02 -0300

    added download button and improved JS implementation

--- expand.js
+++ expand.js
@@ -1,12 +1,20 @@
-function contExpand (elem,height_val) {
-	height_val = height_val.toString();
+function set_att (cls) {
+	var x = document.getElementsByClassName(cls);
+	for (xid='',i=0;i<x.length;i++,xid=''){
+		xid = '"' + x[i].id + '_cont"';
+		x[i].setAttribute('onmouseover','contExpand('+xid+')');
+		x[i].setAttribute('onmouseleave','contHide('+xid+')');
+	}
+} //Sets attributes for expandable elements!
+
+function contExpand (elem) {
 	elem = elem.toString();
-	document.getElementById(elem).style.height = height_val + "px";
-}
+	document.getElementById(elem).style.height = "auto"
+} //Expands HTML element vertically
 
 function contHide (elem) {
 	elem = elem.toString();
 	document.getElementById(elem).style.height = "0px";
-}
+} //Contracts HTML element vertically
 
 
